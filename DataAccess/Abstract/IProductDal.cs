568e6f68d4871483e79a33c6cd939d1b084f04a4
M. MUSTAFA ÇELİK <M. MUSTAFA ELK@DESKTOP-80KA9BE>
2023-03-04 18:28:50 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal
+    {
+        List<Product> GetAll();   
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+
+        //Ürünleri kategoriye göre filtrele
+        List<Product> GetAllByCategory (int categoryId);
+
+
+    }
+}
