a3a1a8539a1f06f4817691412d0015be98e34c94
M. MUSTAFA ÇELİK <M. MUSTAFA ELK@DESKTOP-80KA9BE>
2023-03-06 18:26:00 +0300

    06.03 Ders Sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Abstract;
 using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using System;
@@ -13,18 +14,30 @@ namespace Business.Concrete
     public class ProductManager : IProductService
     {
         IProductDal _productDal;
+        private EfCategoryDal efCategoryDal;
 
         public ProductManager(IProductDal productDal)
         {
             _productDal = productDal;
         }
 
+
         public List<Product> GetAll()
         {
             //İş kodları
             //Yetkisi var mı?
             return _productDal.GetAll();
-              
+
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p=>p.CategoryId==id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p=> p.UnitPrice>=min && p.UnitPrice<=max);
         }
     }
 }
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,28 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint (kısıt-lama)
+    //class : referans tip olabilir demek
+    //IEntity : IEntity olabilir ya da IEntity implemet eden bir nesne olabilir
+    //new() : new'lenebilir olmalı --- IEntity i kullanmamak için yani boş şablonu kullanmamak için interfaceler new kenemediğinden dolayı koyarız
+    //aşağıda bir standart oluşturduk
+    public interface IEntityRepository<T> where T : class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+        //Ürünleri kategoriye göre filtrele
+        //List<T> GetAllByCategory(int categoryId);
+    }
+}
